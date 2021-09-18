d02ff6ca4e2d2c207720cbd2c0d6fd6f63e46817
HunterID <walker7732@gmail.com>
2021-09-18 14:19:18 +0300

    rm file

--- file.js
+++
@@ -1,4 +0,0 @@
-const { Types } = require('mongoose');
-
-const arr = [Types.ObjectId('61438e93ba63985988832708')];
-console.log(arr.some((value) => Types.ObjectId('61438e93ba63985988832708').toString() === value.toString()))
