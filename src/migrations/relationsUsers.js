ae573ca88b7acbe17457411d61a0e1dbe4e3beca
Lutsenko Vladimir <walker7732@gmail.com>
2021-09-20 19:46:42 +0300

    update

--- src/migrations/relationsUsers.js
+++ src/migrations/relationsUsers.js
@@ -10,72 +10,86 @@ const MAX_AMOUNT_NEW_USERS = 100;
 const MAX_AMOUNT_NEW_RELATIONS = 300;
 
 async function randomRelations() {
-    const users = await Users.find({}, { _id: 1 });
+	const users = await Users.find({}, { _id: 1 });
 
-    let createdRelations = 0;
-    while (createdRelations < MAX_AMOUNT_NEW_RELATIONS) {
-        const [firstRandomNumber, secondRandomNumber] = generateTwoRandomNumbers(MAX_AMOUNT_NEW_USERS);
-        const firstRandomUserId = users[firstRandomNumber]._id;
-        const secondRandomUserId = users[secondRandomNumber]._id;
+	let createdRelations = 0;
+	while (createdRelations < MAX_AMOUNT_NEW_RELATIONS) {
+		const [firstRandomNumber, secondRandomNumber] =
+			generateTwoRandomNumbers(MAX_AMOUNT_NEW_USERS);
+		const firstRandomUserId = users[firstRandomNumber]._id;
+		const secondRandomUserId = users[secondRandomNumber]._id;
 
-        const [firstRandomUser, secondRandomUser] = await Promise.all([
-            Users.findOne({ _id: Types.ObjectId(firstRandomUserId) }).lean(),
-            Users.findOne({ _id: Types.ObjectId(secondRandomUserId) }).lean(),
-        ]);
+		const [firstRandomUser, secondRandomUser] = await Promise.all([
+			Users.findOne({ _id: Types.ObjectId(firstRandomUserId) }).lean(),
+			Users.findOne({ _id: Types.ObjectId(secondRandomUserId) }).lean(),
+		]);
 
-        const isBothRandomUsersAreFriends =
-            firstRandomUser.friends.some((id) => secondRandomUserId.toString() === id.toString()) &&
-            secondRandomUser.friends.some((id) => firstRandomUserId.toString() === id.toString());
+		const isBothRandomUsersAreFriends =
+			firstRandomUser.friends.some(
+				id => secondRandomUserId.toString() === id.toString(),
+			) &&
+			secondRandomUser.friends.some(
+				id => firstRandomUserId.toString() === id.toString(),
+			);
 
-        if (isBothRandomUsersAreFriends) {
-            continue;
-        }
+		if (isBothRandomUsersAreFriends) {
+			continue;
+		}
 
-        const isFirstRandomUserFollowedBySecond = firstRandomUser.followers.some((id) => secondRandomUserId.toString() === id.toString());
-        const isSecondRandomUserFollowedByFirst = secondRandomUser.followers.some((id) => firstRandomUserId.toString() === id.toString());
+		const isFirstRandomUserFollowedBySecond = firstRandomUser.followers.some(
+			id => secondRandomUserId.toString() === id.toString(),
+		);
+		const isSecondRandomUserFollowedByFirst = secondRandomUser.followers.some(
+			id => firstRandomUserId.toString() === id.toString(),
+		);
 
-        if (isFirstRandomUserFollowedBySecond && isSecondRandomUserFollowedByFirst) {
-            await Promise.all([
-                Users.updateOne(
-                    { _id: firstRandomUser._id },
-                    {
-                        $pull: { followers: Types.ObjectId(secondRandomUserId) },
-                        $push: { friends: Types.ObjectId(secondRandomUserId) },
-                    },
-                ),
-                Users.updateOne(
-                    { _id: secondRandomUser._id },
-                    {
-                        $pull: { followers: Types.ObjectId(firstRandomUserId) },
-                        $push: { friends: Types.ObjectId(firstRandomUserId) },
-                    },
-                ),
-            ]);
+		if (
+			isFirstRandomUserFollowedBySecond &&
+			isSecondRandomUserFollowedByFirst
+		) {
+			await Promise.all([
+				Users.updateOne(
+					{ _id: firstRandomUser._id },
+					{
+						$pull: { followers: Types.ObjectId(secondRandomUserId) },
+						$push: { friends: Types.ObjectId(secondRandomUserId) },
+					},
+				),
+				Users.updateOne(
+					{ _id: secondRandomUser._id },
+					{
+						$pull: { followers: Types.ObjectId(firstRandomUserId) },
+						$push: { friends: Types.ObjectId(firstRandomUserId) },
+					},
+				),
+			]);
 
-            ++createdRelations;
-            continue;
-        }
+			++createdRelations;
+			continue;
+		}
 
-        await Users.updateOne({ _id: firstRandomUser._id }, { $push: { followers: Types.ObjectId(secondRandomUserId) } });
-        ++createdRelations;
-        continue;
-    }
+		await Users.updateOne(
+			{ _id: firstRandomUser._id },
+			{ $push: { followers: Types.ObjectId(secondRandomUserId) } },
+		);
+		++createdRelations;
+		continue;
+	}
 }
 
 function generateTwoRandomNumbers(number = 0) {
-    const firstNumber = randomNumber(number);
-    let secondNumber = randomNumber(number);
+	const firstNumber = randomNumber(number);
+	let secondNumber = randomNumber(number);
 
-    while (firstNumber === secondNumber) {
-        secondNumber = randomNumber(number);
-    }
+	while (firstNumber === secondNumber) {
+		secondNumber = randomNumber(number);
+	}
 
-    return [firstNumber, secondNumber];
+	return [firstNumber, secondNumber];
 }
 
 function randomNumber(number) {
-    return Math.floor(Math.random() * number);
+	return Math.floor(Math.random() * number);
 }
 
-randomRelations()
-.then(() => process.exit(1));
+randomRelations().then(() => process.exit(1));
